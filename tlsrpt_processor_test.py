80f133f62f13d58d19079cca5494751e0c94f2c7
Daniel Margolis <dan@af0.net>
2018-08-05 10:15:42 +0000

    Add a TODO for Alex.

--- tlsrpt_processor_test.py
+++ tlsrpt_processor_test.py
@@ -3,6 +3,7 @@ import tlsrpt_processor
 
 class TestParsesSample(unittest.TestCase):
   def test_rfc_sample(self):
+    # TODO(abrotman): Check the output? ;)
     tlsrpt_processor.main(["-i", "testdata/sample.json", "-o", "kv"])
 
 
